2c6d3c482049d560ae49c4f96aae04bcc506cfde
Ivy Chenyao <ivychenyao@ivys-air.coldspring.marian.edu>
2017-07-12 11:17:35 -0400

    Add Google Maps logo

--- src/Maps.js
+++ src/Maps.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import './Maps.css'
 
 class Maps extends Component {
     render() {
         return (
-            <div>
-                <input type="text" value="Hi" />
+            <div className="maps">
+                <img 
+                    className="maps-logo" 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Google_Maps.svg/2000px-Google_Maps.svg.png"
+                    alt="maps logo"
+                />
             </div>
         )
     }
 }
 
 export default Maps
+
